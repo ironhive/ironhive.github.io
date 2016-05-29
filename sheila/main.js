0eee74f5adca73a6a0b8571fcb52e7a3dbcb1473
Bryan <ironhive@gmail.com>
2016-05-29 07:55:21 -0500

    Added transition effect

--- sheila/main.js
+++ sheila/main.js
@@ -30,50 +30,64 @@ $(document).ready(function(){
         ["Sai", "Tie Cha Chuan"]
     ];
 
+    var activityLength = 5000;
+
     var init = function() {
 
-        // Hide activity
+        // Hide activity setup
         $('.activity-duration').hide();
         $('.activity-selection').hide();
+
+        // Hide activity
+        //$('.activity-type').hide();
 
         // Get practice options
         selectOptions();
 
-        setInterval(
-            function(){
-                // Select activity
-                var activity = Math.floor(Math.random()*4);
-                var name = "";
-                var count = "";
-                var description = "";
-                switch (activity) {
-                    case 0:
-                        name = activityTypes[activity];
-                        description = shortKatas[Math.floor(Math.random()*30)];
-                        break;
-                    case 1:
-                        name = activityTypes[activity];
-                        description = sparringTechniques[Math.floor(Math.random()*20)];
-                        break;
-                    case 2:
-                        var entry = [Math.floor(Math.random()*2)];
-                        name = longForms[entry][0];
-                        description = longForms[entry][1];
-                        break;
-                    case 3:
-                        var entry = [Math.floor(Math.random()*2)];
-                        name = weaponForms[entry][0];
-                        description = weaponForms[entry][1];
-                        break;
-                }
-                $('.activity-type-title').text(name);
-                $('.activity-type-number').text(count);
-                $('.activity-type-description').text(description);
-            },
-            4000  /* 10000 ms = 10 sec */
-        );
+        // Start showing activities
+        activityFlow();
+        setInterval(activityFlow, 5000);
 
+    };
 
+    var activityFlow = function(){
+        $('.activity-type').addClass('change');
+        var timeout = setTimeout(getActivity, 400);
+    };
+
+    var getActivity = function(){
+
+        // Select activity
+        var activity = Math.floor(Math.random()*4);
+        var name = "";
+        var count = "";
+        var description = "";
+        switch (activity) {
+            case 0:
+                name = activityTypes[activity];
+                description = shortKatas[Math.floor(Math.random()*30)];
+                break;
+            case 1:
+                name = activityTypes[activity];
+                description = sparringTechniques[Math.floor(Math.random()*20)];
+                break;
+            case 2:
+                var entry = [Math.floor(Math.random()*2)];
+                name = longForms[entry][0];
+                description = longForms[entry][1];
+                break;
+            case 3:
+                var entry = [Math.floor(Math.random()*2)];
+                name = weaponForms[entry][0];
+                description = weaponForms[entry][1];
+                break;
+        }
+        $('.activity-type-title').text(name);
+        $('.activity-type-number').text(count);
+        $('.activity-type-description').text(description);
+
+        // Animate out
+        $('.activity-type').removeClass('change');
     };
 
     var selectOptions = function() {
